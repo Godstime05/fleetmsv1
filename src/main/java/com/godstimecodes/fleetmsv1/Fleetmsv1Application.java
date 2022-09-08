e5e298aec74364af028700f0cf30d38c0a4d673b
Godstime Inibehe Ekanem <67835386+Godstime05@users.noreply.github.com>
2022-09-08 09:52:46 +0100

    First Commit

---
+++ src/main/java/com/godstimecodes/fleetmsv1/Fleetmsv1Application.java
@@ -0,0 +1,13 @@
+package com.godstimecodes.fleetmsv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fleetmsv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fleetmsv1Application.class, args);
+	}
+
+}
